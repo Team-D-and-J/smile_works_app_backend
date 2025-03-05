63a5e1b7cbfa7506d720537fb1ae8d394b15f18c
Leon Lam <leonlam34@gmail.com>
2025-03-05 12:18:24 -0600

    updated appointment and inventory to reflect schema

--- schemas/schema.appointment.js
+++ schemas/schema.appointment.js
@@ -1,13 +1,28 @@
 const schema = require('mongoose').schema;
 
 const definition = {
-	"_id": String,
-	"userId": {
+	"_appointmentId": String,
+	"dentistId": {
+		"type": String,
+		"required": String,
+	},
+	"patientId": {
 		"type": String,
 		"required": true,
 	},
-	"time": Date,
+	"date": {
+		"type": Date,
+		"required": true,
+	},
+	"time": { // Format -> HH:mm
+		"type": String,
+		"required": true,
+	},
 	"treatmentMaster": String,
+	"status": {
+		"type": Boolean,
+		"required": true,
+	},
 	_metadata: {
 		"createdAt": {
 			"type": Date,
