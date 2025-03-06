634855af3801182bf3618701a566cc928f87068d
pkasu06 <pkasu06@gmail.com>
2025-03-05 20:04:09 -0600

    Added Schema for Users

---
+++ schemas/schema.user.js
@@ -0,0 +1,79 @@
+const schema = require('mongoose').schema;
+
+const definition = {
+	"_id": String,
+	"name": {
+		"type": String,
+		"required": true,
+	},
+	"password": {
+		"type": String,
+		"required": true,
+	},
+	"salt": {
+        "type": String,
+        "required": true,
+    },
+    "orgID": {
+        "type": schema.Types.ObjectId,
+		"ref": "Organization",
+        "required": true,
+    },
+    "roles": {
+        "isPatient": {
+			"type": Boolean,
+			"required": false,
+		},
+		"isDoctor": {
+			"type": Boolean,
+			"required": false,
+		},
+		"isNurse": {
+			"type": Boolean,
+			"required": false,
+		},
+    },
+    "notificationPreference": {
+        "type": Boolean,
+        "required": false,
+    },
+	"phoneNumber": {
+		"type": Number,
+		"required": true,
+	},
+    "email": {
+        "type": String,
+		"required": true,
+		"unique": true
+    },
+    "billingDetails": {
+        "type": String,
+		"required": true,
+    },
+	_metadata: {
+		"createdAt": {
+			"type": Date,
+			"default": new Date()
+		},
+		"lastUpdatedAt": {
+			"type": Date,
+			"required": true,
+		},
+		"updatedBy": {
+			"type": String,
+			"required": true,
+
+		},
+		"isDeleted": {
+			"type": Boolean,
+			"default": false,
+		},
+		"version": {
+			"type": Number,
+			"default": 1
+		}
+	}
+
+}
+
+module.exports = schema(definition);
