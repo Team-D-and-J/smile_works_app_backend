72dd53ae4e09728be15082c4b1755415dccb01fa
pkasu06 <pkasu06@gmail.com>
2025-03-06 12:10:54 -0600

    Treatment schema, crud api

---
+++ schemas/schema.treatment.js
@@ -0,0 +1,66 @@
+const schema = require("mongoose").Schema;
+
+const treatementSchema = {
+    treatmentId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    organizationId: {
+        type: String,
+        required: true,
+    },
+    patientId: {
+        type: String,
+        required: true,
+    },
+    doctorId: {
+        type: String,
+        required: true,
+    },
+    treatmentMasterId: {
+        type: String,
+        required: true,
+    },
+    steps: {
+        type: [String], 
+        required: true,
+    },
+    files: {
+        type: [String], 
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    typeOfFile: {
+        type: String,
+        enum: ['doc', 'img', 'video', 'audio'], 
+        required: true,
+    },
+    _metadata: {
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        lastUpdatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedBy: {
+            type: String,
+            required: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        version: {
+            type: Number,
+            default: 1,
+        },
+    },
+};
+
+module.exports = schema(treatementSchema);
