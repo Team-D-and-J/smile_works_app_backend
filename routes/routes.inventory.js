c3786b1f52f04089377f8efd90bf1cfa24bb87fa
Leon Lam <leonlam34@gmail.com>
2025-03-07 14:59:29 -0600

    created appointment and inventory APIs

--- routes/routes.inventory.js
+++ routes/routes.inventory.js
@@ -1,6 +1,18 @@
-const router = require('express').Router();
-const init = require('../init');
-const Inventory = require('../schemas/schema.inventory');
+const router = require("express").Router();
+const mongooseCrud = require("mongoose-express-middleware");
+const inventorySchema = require("../schemas/schema.inventory");
 
+const inventoryCollection = "inventory";
+const crud = new mongooseCrud(inventoryCollection, inventorySchema, null);
+
+// Define API routes
+router.get("/", crud.find);
+router.get("/:id", crud.findById);
+router.get("/utils/count", crud.count);
+router.post("/", crud.create);
+router.put("/:id", crud.update);
+router.delete("/:id", crud.deleteById);
+router.delete("/utils/deleteMany", crud.deleteMany);
+router.post("/utils/aggregate", crud.aggregate);
 
 module.exports = router;
