3120ce3037bcc0f844dfbdc8bd2de0f85ad662dc
Thomas Gooch <teg824@hotmail.com>
2025-03-11 12:45:58 -0500

    Update schema to match db schema

--- schemas/schema.productMaster.js
+++ schemas/schema.productMaster.js
@@ -1,52 +1,47 @@
 const schema = require("mongoose").Schema;
 
 const productSchema = {
-    productId: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    catalogNumber: { 
-        type: Number, 
-        required: true 
-    },
-    name: { 
-        type: String, 
-        required: true 
-    },
-    unitOfMeasure: { 
-        type: String, 
-        required: true 
-    },
-    unitPrice: { 
-        type: Number, 
-        required: true 
-    },
-    vendorName: { 
-        type: String
-    },
-    _metadata: {
-        createdAt: { 
-            type: Date, 
-            default: Date.now 
-        },
-        lastUpdatedAt: { 
-            type: Date, 
-            default: Date.now
-        },
-        updatedBy: { 
-            type: String, 
-            required: true 
-        },
-        isDeleted: { 
-            type: Boolean, 
-            default: false 
-        },
-        version: { 
-            type: Number, 
-            default: 1 
-        },
-    },
+	productId: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+
+	name: {
+		type: String,
+		required: true,
+	},
+	unitOfMeasure: {
+		type: String,
+		required: true,
+	},
+	unitPrice: {
+		type: Number,
+		required: true,
+	},
+
+	_metadata: {
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		lastUpdatedAt: {
+			type: Date,
+			default: Date.now,
+		},
+		updatedBy: {
+			type: String,
+			required: true,
+		},
+		isDeleted: {
+			type: Boolean,
+			default: false,
+		},
+		version: {
+			type: Number,
+			default: 1,
+		},
+	},
 };
 
 module.exports = schema(productSchema);
