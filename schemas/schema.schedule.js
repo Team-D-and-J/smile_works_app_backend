c40914649cbc94801b3c5e90dd4df79262a4d0f2
Leon Lam <leonlam34@gmail.com>
2025-03-11 22:15:56 -0500

    created schemas and CRUD APIs for schedule, patient, purhcaseOrders & created schema for organization

---
+++ schemas/schema.schedule.js
@@ -0,0 +1,17 @@
+const schema = require("mongoose").Schema;
+
+const scheduleSchema = {
+	_id: { type: String, required: true },
+	patientId: { type: String, required: true },
+	time: { type: String, required: true },
+	treatment: { type: String, required: true },
+	_metadata: {
+		createdAt: { type: Date, default: Date.now },
+		lastUpdatedAt: { type: Date, default: Date.now, required: true },
+		updatedBy: { type: String, required: true },
+		isDeleted: { type: Boolean, default: false },
+		version: { type: Number, default: 1 },
+	},
+};
+
+module.exports = schema(scheduleSchema);
